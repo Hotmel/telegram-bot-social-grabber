1c2dfd6f04c89476631c70fa6c45e8155db559d4
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2016-08-13 19:32:58 +0700

    Добавил описание файлов

--- pgmodule.py
+++ pgmodule.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import psycopg2,sys,config
 
-## Модуль с методами для работы с БД PostgreSQL
+## 
+# \file 
+# \brief Модуль для работы с базой данных PostgreSQL
+# \authors Борис Бочкарев http://vk.com/id23382988
+# \version 0.1
 
-## Метод для получения токена
+## 
+# Метод для получения токена
+# \param id_chat ID чата из Telegram
+# \param social Социальная сеть, например 'vk'
+# \return Токен для данной социальной сети данного чата
 def getToken(id_chat, social):
 	if social == 'vk':
 		connect = psycopg2.connect(user=config.userDB, password=config.passwordDB, host='127.0.0.1', database='socialcollector', port=5432)
--- vkmodule.py
+++ vkmodule.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 import config, pgmodule, requests, json
 
-## Класс для работы с соц.сетью ВК
+## 
+# \file 
+# \brief Модуль для работы с социальной сетью ВКонтакте
+# \authors Борис Бочкарев http://vk.com/id23382988
+# \version 0.1
 
-## метод генерации ссылки для авторизации в ВК
+## 
+# Метод генерации ссылки для авторизации в ВК
+# \param chat_id ID чата из Telegram
+# \return Ссылку для авторизации в текстовом виде
 def vkauthstr(chat_id):
 	textMsg = '''Для авторизации в ВК вам необходимо пройти по ссылке:\n
 	http://oauth.vk.com/authorize?client_id='''+str(config.idAppVK)+'''&scope=friends&redirect_uri=http://y909539n.bget.ru/vkauth.php&display=mobile&response_type=token&state='''+str(chat_id)
 	return textMsg
 
-## метод для получения списка друзей
+## 
+# Метод для получения списка друзей
+# \param token Токен, полученные от VK.API
+# \return Список всех друзей со списками вида: [id_пользователя,Фамилия,Имя]
 def vkfriends(token):
 	response = requests.get("https://api.vk.com/method/friends.get?access_token="+str(token)+"&offset=0&order=name&fields=[name]&name_case=nom")
 	arrayResp = json.loads(response.content.decode("UTF-8"))["response"]
@@ -17,9 +27,12 @@ def vkfriends(token):
 	for friend in arrayResp:
 		localArray = [friend["uid"],friend["first_name"],friend["last_name"]]
 		friends.append(localArray)
-	return friends # возвращает список всех друзей со списками вида: id_пользователя,Фамилия,Имя
+	return friends 
 
-## метод для получения записей со стены пользователей
+## 
+# Метод для получения записей со стены пользователей
+# \param user_id ID пользователя в ВК
+# \return Список из 100 записей со стены пользователя со списками вида: [id_автора,текст_записи]
 def vkwall(user_id):
 	response = requests.get("https://api.vk.com/method/wall.get?offset=0&owner_id="+str(user_id)+"&count=100&filter=owner")
 	arrayResp = json.loads(response.content.decode("UTF-8"))["response"]
@@ -27,4 +40,4 @@ def vkwall(user_id):
 	for item in arrayResp:
 		if type(item) != int and str(item["text"]) != "":
 			wall.append([item["from_id"],item["text"]])
-	return wall # возвращает список 100 записей со стены пользователя со списками вида: id_автора,текст_записи
+	return wall
